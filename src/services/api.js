1564c7d6c87e0d71a5cf091774e138e0263e98ab
Ian Augusto Silva <ian.silva@venturus.org.br>
2021-09-09 17:13:03 -0300

    changed "base URL" in "api.js"

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  // baseURL: "http://localhost:3333",
+  baseURL: "https://vnt-employee-back.herokuapp.com/",
 });
 
 export default api;
